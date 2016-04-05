60ac1d5dfc617fbad3689a28673e7f255ce9512a
Gibbon <ad4m@outlook.com>
2016-04-05 22:21:56 +0200

    Fixed completely

--- sysdep.c
+++ sysdep.c
@@ -2,4 +2,24 @@
 #include "io.h"
 #include "sysdep.h"
 
-nap(2000);
+#ifdef WINDOWS
+#include <windows.h>
+#elif _POSIX_C_SOURCE
+#include <time.h>
+#else
+#include <unistd.h>
+#endif
+
+void nap(int milliseconds)
+{
+#ifdef WINDOWS
+    Sleep(milliseconds);
+#elif _POSIX_C_SOURCE
+    struct timespec tmsc;
+    tmsc.tv_sec = milliseconds / 1000;
+    tmsc.tv_nsec = (milliseconds % 1000) * 1000000;
+    nanosleep(&tmsc, NULL);
+#else
+    usleep(milliseconds * 1000);
+#endif
+}
