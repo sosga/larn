b0b7203cb7ce9bc3d486aa96106405cab781aa08
Gibbon <gibbon@gibbon>
2017-03-20 14:59:43 +0100

    added non-cmake build scripts for lazy folks ;)

---
+++ build/build_debug_lazy.sh
@@ -0,0 +1,3 @@
+g++ -pipe -Wall -Wextra -pedantic -Wpointer-arith -Wshadow -Wmissing-declarations -Wredundant-decls -g ../src/*.cpp -lm -lncurses
+mv ../src/a.out larn_101d
+mv ../src/larn_101d ..
---
+++ build/build_release_lazy.sh
@@ -0,0 +1,3 @@
+g++ -pipe -Wall -Werror -fomit-frame-pointer ../src/*.cpp -lm -lncurses
+mv ../src/a.out larn_101r
+mv ../src/larn_101r ..
