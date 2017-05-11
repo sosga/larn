36ca00e8b7c096a0883d99a579d00f6a6e1cd4c5
Gibbon <atsb@users.noreply.github.com>
2017-05-11 13:56:03 +0200

    Another file for #61

--- src/config.cpp
+++ src/config.cpp
@@ -1,27 +1,33 @@
+/* Copyright 2016 Gibbon aka 'atsb'
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+   
+       http://www.apache.org/licenses/LICENSE-2.0
+       
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 /*
- *  config.c    --  This defines the installation dependent variables.
- *                  Some strings are modified later.  ANSI C would
- *                  allow compile time string concatenation, we must
- *                  do runtime concatenation, in main.
+ *  config.cpp	This defines the installation dependent variables.
+ *              Some strings are modified later.  ANSI C would
+ *              allow compile time string concatenation, we must
+ *              do runtime concatenation, in main.
  */
 #include "../includes/larncons.h"
 #include "../includes/larndata.h"
 #include "../includes/larnfunc.h"
 
 #ifndef WIZID
-	#define WIZID   0
+	#define WIZID	0
 #endif
 
-/*
- *  All these strings will be appended to in main() to be complete filenames
- */
-
-
-/* Make LARNHOME readable from the larnopt file into a lardir variable.
- */
 char scorefile[PATHLEN];
 char logfile[PATHLEN];
 char mazefile[PATHLEN];
 char fortfile[PATHLEN];
 char playerids[PATHLEN];
-char diagfile[PATHLEN];		/* the diagnostic filename  */
