e34fccc1749ab0c935e6bf4743e539d6f00dfc7f
Gibbon <atsb@users.noreply.github.com>
2017-05-15 16:48:41 +0200

    Update io.cpp

--- src/io.cpp
+++ src/io.cpp
@@ -81,7 +81,7 @@
 #include "../includes/io.h"
 #include "../includes/monster.h"
 #include "../includes/scores.h"
-#include "termcap/tgoto.hpp"
+#include "termcap/termcap.hpp"
 #include "../includes/larncons.h"
 #include "../includes/larndata.h"
 #include "../includes/larnfunc.h"
