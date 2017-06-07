23a5ca53fb0e55dfa98e90b42f2a12259dd5ad30
Gibbon <atsb@users.noreply.github.com>
2017-06-07 11:49:20 +0200

    Delete build_debug_lazy.bat

--- build/build_debug_lazy.bat
+++
@@ -1,1 +0,0 @@
-g++ -pipe -Wall -Wextra -pedantic -Wpointer-arith -Wshadow -Wmissing-declarations -Wredundant-decls -g -DWINDOWS -DMULTIPLE_SCORE_ENTRY -DTERM_DARK_BACKGROUND ../src/*.cpp -lm -lcurses
